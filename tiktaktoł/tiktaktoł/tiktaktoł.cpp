60829fea51ddb48fa61863d5ae12bc8e21941d77
Nezxie <49255694+Nezxie@users.noreply.github.com>
2019-06-05 20:08:12 +0200

    Add files via upload

--- tiktaktoł/tiktaktoł/tiktaktoł.cpp
+++ tiktaktoł/tiktaktoł/tiktaktoł.cpp
@@ -14,7 +14,7 @@ using namespace std;
 //Instrukcja
 void Instrukcja()
 {
-	printf("\t\t\t Kolko i krzyzyk\n\n"); //\t to akapit
+	printf("\t\t\t Kolko i krzyzyk\n\n"); //\t to akapit \n nowa linia
 	printf("Aby wstawic symbol w dana komorke wybierz numer od 1-9");
 	printf("\t\t\t  1 | 2  | 3  \n");
 	printf("\t\t\t--------------\n");
@@ -22,6 +22,19 @@ void Instrukcja()
 	printf("\t\t\t--------------\n");
 	printf("\t\t\t  7 | 8  | 9  \n\n");
 	printf("-\t-\t-\t-\t-\t-\t-\t-\t-\t-\n\n");
+}
+//inicjalizacja planszy
+void UtworzPlansze(char plansza[][BOK], int ruchy[])
+{
+	//czysci plansze ze smieci
+	for (int i = 0; i < BOK; i++)
+	{
+		for (int j = 0; j < BOK; j++)
+			plansza[i][j] = ' '; //pusta tablica
+	}
+	//wypelnienie tab ruchow
+	for (int i = 0; i < BOK*BOK; i++)
+		ruchy[i] = i;
 }
 //obecny stan planszy
 void PokazPlansze(char plansza[][BOK]) //plansza jest tabilca charow - jak widac
@@ -38,10 +51,144 @@ void PokazPlansze(char plansza[][BOK]) //plansza jest tabilca charow - jak widac
 		plansza[2][1], plansza[2][2]);
 	
 }
+//okreslenie zwyciezcy
+void Zwyciezca(int KogoTura) 
+{
+	if (KogoTura == PC) //sprawdzamy czyja jest tura
+		cout << "Komputer wygrał!" << endl; 
+	else
+		cout << "Gracz wygrał!" << endl;
+}
+//funkcje sprawdzajace warunek wygranej - 3 dla 3 kierunkow
+
+//poziomy
+bool Poziom(char plansza[][BOK])
+{
+	int c; //licznik ile w rzedzie jest takich samych znakow
+	for (int i = 0; i < BOK; i++)
+	{
+	
+		for (int j = 1; j < BOK; j++)
+		{
+			if (plansza[i][0] == plansza[i][j] && plansza[i][0]!=' ') c++; //zabezpieczamy przed wykryciem wyhranej gdy caly rzad pusty
+			
+		}
+		if (c == BOK-1)
+			return(true);
+	}
+	return (false);
+}
+//sprawdzanie w pionie
+bool Pion(char plansza[][BOK])
+{
+	int c; //licznik ile w kolumnie jest takich samych znakow
+	for (int i = 0; i < BOK; i++)
+	{
+
+		for (int j = 1; j < BOK; j++)
+		{
+			if (plansza[0][i] == plansza[j][i] && plansza[0][i] != ' ') c++;
+
+		}
+		if (c == BOK-1)
+			return(true);
+	}
+	return (false);
+}
+
+//sprawdzanie przekatnych
+bool Diag(char plansza[][BOK])
+{
+	int c; //licznik ile w przekatnej jest takich samych znakow
+	int i = 0;
+
+		for (int j = BOK; j >0; j--)
+		{
+			if (plansza[j][i] == plansza[j-1][i+1] && plansza[j][i] != ' ') c++; //lewa-dolna przekatna
+			i++;
+		}
+		if (c == BOK-1)
+			return(true);
+
+	
+
+	
+		i = 0;
+		for (int j = 1; j <BOK ; j++)
+		{
+			if (plansza[i][i] == plansza[j][j] && plansza[j][j] != ' ') c++; //lewa-gorna przekatna
+			i++;
+		}
+		if (c == BOK - 1)
+			return(true);
+
+	return (false);
+}
+
+//kiedy koniec gry
+
+bool KoniecGry(char plansza[][BOK])
+{
+	if (Poziom || Pion || Diag)
+		return(true);
+	else
+		return(false);
+}
+
+// faktyczna gra!!
+
+void Graj(int KogoTura)
+{
+	char plansza[BOK][BOK];
+	int ruchy[BOK*BOK];
+	
+
+	UtworzPlansze(plansza, ruchy);
+	Instrukcja();
+	int licznik = 0, x, y; //??????
+
+	while (KoniecGry(plansza) == false && licznik != BOK * BOK)
+	{
+		if (KogoTura == PC)
+		{
+			x = ruchy[licznik] / BOK;
+			y = ruchy[licznik] % BOK;
+			plansza[x][y] = PC_RUCH;
+			printf("Komputer wstawił %c w pole %d\n", PC_RUCH, ruchy[licznik] + 1);
+			PokazPlansze(plansza);
+			licznik++;
+			KogoTura = GRACZ;
+		}
+		else if (KogoTura == GRACZ)
+		{
+			x = ruchy[licznik] / BOK;
+			y = ruchy[licznik] % BOK;
+			plansza[x][y] = GRACZ_RUCH;
+			printf("Wstawiłeś %c w pole %d\n", PC_RUCH, ruchy[licznik] + 1);
+			PokazPlansze(plansza);
+			licznik++;
+			KogoTura = PC;
+		}
+	}
+
+	//w przypadku remisu
+	if (KoniecGry(plansza) == false && licznik == BOK * BOK)
+		cout << "Remis!" << endl;
+	else
+	{
+		if (KogoTura == PC)  //zamiana graczy zeby wpisac zwyciezce
+			KogoTura = GRACZ;
+		else if (KogoTura == GRACZ)
+			KogoTura =PC;
+		Zwyciezca(KogoTura);
+	}
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Graj(PC); //kto zaczyna
+
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
